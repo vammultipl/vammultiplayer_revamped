ddcecd550b9302220983bbef8e22167ed26ad67f
vammultipl <vammultipl@gmail.com>
2024-06-12 04:14:20 +0000

    Update help message

--- discord_registrationbot.go
+++ discord_registrationbot.go
@@ -113,7 +113,7 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 		url := "https://www.google.com/search?q=google+what+is+my+ip"
 		text := fmt.Sprintf("Unknown command. Here are the commands you can use:\n\n" +
 		"1. `/register <IP>` - Register your IP address with the VaM multiplayer server. This will gain you entry to the server with about 2 weeks expiration. If you cannot connect to the server in VaM, register again. To find your IP, visit the link below. Link:\n%s\n\n" +
-		    "2. `/state` - Check the current game status to see who is playing.\n\n" +
+		    "2. `/state` - Check the current game status to see who is playing. You can also see the same info in my status on Discord updated every 30s.\n\n" +
 		    "Please use one of the above commands.\n", url)
 		s.ChannelMessageSend(m.ChannelID, text)
 	}
